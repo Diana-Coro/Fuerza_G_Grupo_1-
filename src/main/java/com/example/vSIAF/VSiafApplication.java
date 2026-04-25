cc59614a4b28d8fcbe636aee6f980396992220db
Diana Coro Llave <esmeralda21diana@gmail.com>
2026-04-25 00:00:35 -0400

    inicio

---
+++ src/main/java/com/example/vSIAF/VSiafApplication.java
@@ -0,0 +1,13 @@
+package com.example.vSIAF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VSiafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VSiafApplication.class, args);
+	}
+
+}
