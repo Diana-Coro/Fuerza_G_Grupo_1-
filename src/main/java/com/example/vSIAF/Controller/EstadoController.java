d630ec61c248da532769862ec137fe2c2afa4c2f
armandacho <armandoayaarmando@gmail.com>
2026-05-08 22:39:46 -0400

    Subir Estado

---
+++ src/main/java/com/example/vSIAF/Controller/EstadoController.java
@@ -0,0 +1,78 @@
+package com.example.vSIAF.Controller;
+
+import com.example.vSIAF.Estado;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/estado")
+public class EstadoController {
+
+    List<Estado> lista = new ArrayList<>();
+
+    // DATOS INICIALES
+    public EstadoController() {
+
+        lista.add(new Estado(1, "Bueno"));
+        lista.add(new Estado(2, "Regular"));
+        lista.add(new Estado(3, "Malo"));
+    }
+
+    // GET -> LISTAR
+    @GetMapping
+    public List<Estado> listar() {
+        return lista;
+    }
+
+    // GET POR ID
+    @GetMapping("/{id}")
+    public Estado buscar(@PathVariable Integer id) {
+
+        for (Estado e : lista) {
+            if (e.getCodestado().equals(id)) {
+                return e;
+            }
+        }
+
+        return null;
+    }
+
+    // POST -> INSERTAR
+    @PostMapping
+    public Estado insertar(@RequestBody Estado estado) {
+
+        lista.add(estado);
+
+        return estado;
+    }
+
+    // PUT -> ACTUALIZAR
+    @PutMapping("/{id}")
+    public Estado actualizar(@PathVariable Integer id,
+                             @RequestBody Estado nuevo) {
+
+        for (Estado e : lista) {
+
+            if (e.getCodestado().equals(id)) {
+
+                e.setNomestado(nuevo.getNomestado());
+
+                return e;
+            }
+        }
+
+        return null;
+    }
+
+    // DELETE
+    @DeleteMapping("/{id}")
+    public String eliminar(@PathVariable Integer id) {
+
+        lista.removeIf(e -> e.getCodestado().equals(id));
+
+        return "Eliminado";
+    }
+}
