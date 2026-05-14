f35074b7482d8d78fc91650c44b3e1382e3bb7fb
alfredomc22071999-cyber <alfredomc22071999@gmail.com>
2026-05-14 00:03:30 -0400

    v.2.1.4

--- src/test/java/com/example/vSIAF/VSiafApplicationTests.java
+++ src/test/java/com/example/vSIAF/VSiafApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.Cta_par;
+package com.example.vSIAF;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CtaParApplicationTests {
+class VSiafApplicationTests {
 
 	@Test
 	void contextLoads() {
