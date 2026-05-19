42734d64b0b52b0926cf3053e5695ebd5ece224f
Diana Coro Llave <esmeralda21diana@gmail.com>
2026-05-19 05:50:30 -0400

    eliminar archivos mes

--- src/main/java/com/example/vSIAF/repository/MesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.vSIAF.repository;
-
-import com.example.vSIAF.entity.MesEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MesRepository extends JpaRepository<MesEntity, Integer> {
-}
