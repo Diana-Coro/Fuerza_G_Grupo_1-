10c8eabc0d02611fdfbe29d294d775c38012a7e3
samira123 <beniasamira123@gmail.com>
2026-05-20 16:04:34 -0400

    Implemento CRUD UnidadAdmin con BD

---
+++ src/main/java/com/example/vSIAF/entity/UnidadAdminEntity.java
@@ -0,0 +1,60 @@
+package com.example.vSIAF.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "unidad_admin")
+public class UnidadAdminEntity {
+
+    private Long entidad;
+
+    @Id
+    private Long unidad;
+
+    private String descrip;
+    private String ciudad;
+
+    public UnidadAdminEntity() {
+    }
+
+    public UnidadAdminEntity(Long entidad, Long unidad, String descrip, String ciudad) {
+        this.entidad = entidad;
+        this.unidad = unidad;
+        this.descrip = descrip;
+        this.ciudad = ciudad;
+    }
+
+    public Long getEntidad() {
+        return entidad;
+    }
+
+    public void setEntidad(Long entidad) {
+        this.entidad = entidad;
+    }
+
+    public Long getUnidad() {
+        return unidad;
+    }
+
+    public void setUnidad(Long unidad) {
+        this.unidad = unidad;
+    }
+
+    public String getDescrip() {
+        return descrip;
+    }
+
+    public void setDescrip(String descrip) {
+        this.descrip = descrip;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+}
