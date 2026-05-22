c290ade62506d28afaabec6606d3b7d2d54d2e19
alfredomc22071999-cyber <alfredomc22071999@gmail.com>
2026-05-22 00:08:48 -0400

    v.2.8

--- src/main/java/com/example/vSIAF/model/CtaPar.java
+++ src/main/java/com/example/vSIAF/model/CtaPar.java
@@ -1,20 +1,33 @@
 package com.example.vSIAF.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class CtaPar {
 
     private Integer id;
+
+    @JsonProperty("Codcont") 
     private Integer codcont;
+
+    @JsonProperty("Partida") 
+    private Integer partida;
+
+    @JsonProperty("Gestion") 
     private Integer gestion;
 
+    
     public CtaPar() {
     }
 
-    public CtaPar(Integer id, Integer codcont, Integer gestion) {
+    
+    public CtaPar(Integer id, Integer codcont, Integer partida, Integer gestion) {
         this.id = id;
         this.codcont = codcont;
+        this.partida = partida;
         this.gestion = gestion;
     }
 
+    // Getters y Setters
     public Integer getId() {
         return id;
     }
@@ -29,6 +42,14 @@ public class CtaPar {
 
     public void setCodcont(Integer codcont) {
         this.codcont = codcont;
+    }
+
+    public Integer getPartida() {
+        return partida;
+    }
+
+    public void setPartida(Integer partida) {
+        this.partida = partida;
     }
 
     public Integer getGestion() {
