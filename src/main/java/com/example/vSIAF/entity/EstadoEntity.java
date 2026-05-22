ce316a4cd9076998c336bbb464d956f35b92c6a8
armandacho <armandoayaarmando@gmail.com>
2026-05-21 23:43:05 -0400

    se modifico los getters y setters

--- src/main/java/com/example/vSIAF/entity/EstadoEntity.java
+++ src/main/java/com/example/vSIAF/entity/EstadoEntity.java
@@ -10,31 +10,31 @@ public class EstadoEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Schema(hidden = true)
-    private Integer Codestado;
+    private Integer codestado;
 
-    private String Nomestado;
+    private String nomestado;
 
     public EstadoEntity() {
     }
 
-    public EstadoEntity(Integer Codestado, String Nomestado) {
-        this.Codestado = Codestado;
-        this.Nomestado = Nomestado;
+    public EstadoEntity(Integer codestado, String nomestado) {
+        this.codestado = codestado;
+        this.nomestado = nomestado;
     }
 
     public Integer getCodestado() {
-        return Codestado;
+        return codestado;
     }
 
-    public void setCodestado(Integer Codestado) {
-        this.Codestado = Codestado;
+    public void setCodestado(Integer codestado) {
+        this.codestado = codestado;
     }
 
     public String getNomestado() {
-        return Nomestado;
+        return nomestado;
     }
 
-    public void setNomestado(String Nomestado) {
-        this.Nomestado = Nomestado;
+    public void setNomestado(String nomestado) {
+        this.nomestado = nomestado;
     }
 }
