99cc54e1fe21e56355ad591ddc275a9addf52bc5
Diana Coro Llave <esmeralda21diana@gmail.com>
2026-06-15 01:36:53 -0400

    Primer proyecto React Grupo Fuerza G

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+
+      <div className="container">
+        <Sidebar />
+
+        <main className="content">
+          <NavBar />
+
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </main>
+      </div>
+
+      <Footer />
+    </BrowserRouter>
+  );
+}
+
+export default App;
